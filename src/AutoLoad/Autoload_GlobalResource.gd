3592a168a4bfab373ec531646f51363efd34c98a
dekhadmai <bank_kuna@hotmail.com>
2022-07-31 11:32:37 -0400

    Add Global stuff to AutoLoad
    GlobalResource for read-only reference
    TransientData for persistence variables

---
+++ src/AutoLoad/Autoload_GlobalResource.gd
@@ -0,0 +1,9 @@
+extends Node
+
+
+# Global reference. Read-only stuff. Use Autoload_TransientData for read-write/temp data
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
