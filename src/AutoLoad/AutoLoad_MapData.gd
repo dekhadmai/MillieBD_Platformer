bbfc0567207ad4bda2b2e88b16603b6aac9d1294
dekhadmai <bank_kuna@hotmail.com>
2022-08-22 03:06:16 -0400

    Random Level Generator (WIP) #1

--- src/AutoLoad/AutoLoad_MapData.gd
+++ src/AutoLoad/AutoLoad_MapData.gd
@@ -2,13 +2,171 @@ extends Node
 
 onready var test_room = load("res://src/Level/LevelRoom.tscn")
 
+var LevelRoomMap = []
+var GridWidth = 10
+var GridHeight = 5
+var DoorChance = 15
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	for i in GridHeight:
+		LevelRoomMap.append([])
+		for j in GridWidth:
+			LevelRoomMap[i].append(LevelRoomData.new())
+			
+	randomize()
+	#seed(35)
+	GenerateRooms()
+	print("\n\n")
+	print_map()
 	pass # Replace with function body.
 
+func GenerateRooms():
+	# start in the middle
+	LevelRoomMap[2][0].bStartRoom = true
+	Traverse(2, 0, -1, -1)
+	pass
+
+func Traverse(row:int, column:int, from_row: int, from_column: int):
+	
+	print("Traverse : (" + str(row) + "," + str(column) + ")")
+	
+	var room_data:LevelRoomData = LevelRoomMap[row][column]
+	
+	if from_row != -1 :
+		# pop the door open from where we came from
+		if from_row == row and from_column == column-1:
+			room_data.bIsDoorOpened[0] = 1
+		if from_row == row-1 and from_column == column:
+			room_data.bIsDoorOpened[1] = 1
+		if from_row == row and from_column == column+1:
+			room_data.bIsDoorOpened[2] = 1
+		if from_row == row+1 and from_column == column:
+			room_data.bIsDoorOpened[3] = 1
+	
+	if (room_data.bTraversed):
+		return
+	room_data.bTraversed = true
+	
+	var can_traverse = [0,0,0,0] #left, up, right, down | 0 = can't traverse, 1 = can traverse, 2 = force traverse
+	var eligible_options = []
+	
+	##### check and force open door to traverse
+	# check travel left
+	if CanTraverse(row, column-1) :
+		can_traverse[0] = 1
+		eligible_options.append(0)
+	
+	# check travel up
+	if CanTraverse(row-1, column) :
+		can_traverse[1] = 1
+		eligible_options.append(1)
+		
+	# check travel right
+	if CanTraverse(row, column+1) :
+		can_traverse[2] = 1
+		eligible_options.append(2)
+		
+	# check travel down
+	if CanTraverse(row+1, column) :
+		can_traverse[3] = 1
+		eligible_options.append(3)
+		
+	if eligible_options.size() > 0:
+		var force_open_index = eligible_options[randi() % eligible_options.size()]
+		can_traverse[force_open_index] = 2
+		
+	##### actual traverse
+	# travel left
+	if can_traverse[0] > 0 and (can_traverse[0] == 2 or randi() % 100 < DoorChance):
+		room_data.bIsDoorOpened[0] = 1
+		Traverse(row, column-1, row, column)
+	else:
+		if room_data.bIsDoorOpened[0] == 0:
+			room_data.bIsDoorOpened[0] = 2
+		
+	# travel up
+	if can_traverse[1] > 0 and (can_traverse[1] == 2 or randi() % 100 < DoorChance):
+		room_data.bIsDoorOpened[1] = 1
+		Traverse(row-1, column, row, column)
+	else:
+		if room_data.bIsDoorOpened[1] == 0:
+			room_data.bIsDoorOpened[1] = 2
+		
+	# travel right
+	if can_traverse[2] > 0 and (can_traverse[2] == 2 or randi() % 100 < DoorChance):
+		room_data.bIsDoorOpened[2] = 1
+		Traverse(row, column+1, row, column)
+	else:
+		if room_data.bIsDoorOpened[2] == 0:
+			room_data.bIsDoorOpened[2] = 2
+		
+	# travel down
+	if can_traverse[3] > 0 and (can_traverse[3] == 2 or randi() % 100 < DoorChance):
+		room_data.bIsDoorOpened[3] = 1
+		Traverse(row+1, column, row, column)
+	else:
+		if room_data.bIsDoorOpened[3] == 0:
+			room_data.bIsDoorOpened[3] = 2
+	
+	pass
+	
+func CanTraverse(row:int, column:int) -> bool :
+	if column < 0 or column >= GridWidth :
+		return false
+	
+	if row < 0 or row >= GridHeight :
+		return false
+		
+	var room_data:LevelRoomData = LevelRoomMap[row][column]
+	
+	if room_data.bTraversed:
+		return false
+	
+	return true
+	
+func print_map():
+	var result:String = ""
+	for i in GridHeight:
+		for j in GridWidth:
+			var room_data:LevelRoomData = LevelRoomMap[i][j]
+			
+			if !room_data.bStartRoom:
+				result += " "
+			else:
+				result += "S"
+				
+			if room_data.bIsDoorOpened[0] == 1:
+				result += "<"
+			else:
+				result += " "
+				
+			if room_data.bIsDoorOpened[1] == 1:
+				result += "^"
+			else:
+				result += " "
+			
+			if room_data.bIsDoorOpened[2] == 1:
+				result += ">"
+			else:
+				result += " "
+				
+			if room_data.bIsDoorOpened[3] == 1:
+				result += "v"
+			else:
+				result += " "
+				
+			result += " | "
+		result += "\n"
+	
+	print(result)
+	pass
 
 func InitSpawnRooms():
+	
+	
+	
 	var room = test_room.instance()
 	var room2 = test_room.instance()
 	var room3 = test_room.instance()
---
+++ src/Base/LevelRoomData.gd
@@ -0,0 +1,9 @@
+class_name LevelRoomData
+extends Node
+
+var bTraversed: bool = false
+var bStartRoom: bool = false
+var bIsAlreadyCleared: bool = false
+var bIsDoorOpened = [0, 0, 0, 0] # left, up, right, down || 0 = undefined, 1 = open, 2 = close
+var LevelRoomTemplate: String
+var RoomType: String
