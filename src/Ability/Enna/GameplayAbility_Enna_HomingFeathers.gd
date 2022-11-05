2b9ae143eafcf79e66ee089e0366f88643ce1172
dekhadmai <bank_kuna@hotmail.com>
2022-11-04 20:30:12 -0400

    Balance changes #1
    + Enna boss room platform longer (help dashing not falling off platform)
    + Enna homing feathers timing nerf (easier to dodge)
    + Enna HP buff

--- src/Ability/Enna/GameplayAbility_Enna_HomingFeathers.gd
+++ src/Ability/Enna/GameplayAbility_Enna_HomingFeathers.gd
@@ -39,7 +39,7 @@ func SpawnActor() -> void:
 func OnSpawnBullet(bullet):
 	if bullet_spawner_component == bullet_spawner_component3 :
 		bullet.second_move_seconds = 1.5 + move_offset_sec
-		move_offset_sec += 0.2
+		move_offset_sec += 0.1
 		
 	if bullet_spawner_component == bullet_spawner_component2 :
 		bullet.second_move_speed = 300
