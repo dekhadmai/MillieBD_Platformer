d038e03ea18cd685071f6e210396421eaefa5926
dekhadmai <bank_kuna@hotmail.com>
2022-11-07 00:57:30 -0500

    Homing ability now have line of sight check for valid targets

--- src/Ability/Player/GameplayAbility_HomingSwarm.gd
+++ src/Ability/Player/GameplayAbility_HomingSwarm.gd
@@ -1,6 +1,7 @@
 extends "res://src/Ability/Player/GameplayAbility_Weapon_Shotgun.gd"
 
 onready var area_enemy_detection: Area2D = $Area2D_EnemyDetection
+onready var line_of_sight: RayCast2D = $RayCast_LineOfSight
 
 var Targets = []
 
@@ -9,6 +10,11 @@ func Activate():
 	var potential_targets = area_enemy_detection.get_overlapping_areas()
 	for area in potential_targets : 
 		var body = area.GetOwnerObject()
+		line_of_sight.set_cast_to( to_local(body.GetTargetingPosition()) )
+		line_of_sight.force_raycast_update()
+		if line_of_sight.is_colliding() :
+			continue
+			
 		if body.GetTeam() != GlobalFunctions.GetOwnerObject(self).GetTeam():
 			Targets.push_back(body)
 			
