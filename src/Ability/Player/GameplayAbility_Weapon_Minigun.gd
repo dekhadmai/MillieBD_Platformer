eee518a94376a928b9a0ca91da4d63b3731cbb20
dekhadmai <bank_kuna@hotmail.com>
2022-11-10 12:28:14 -0500

    Add Minigun to AirTurret enemy
    + Modify Minigun ability a bit

--- src/Ability/Player/GameplayAbility_Weapon_Minigun.gd
+++ src/Ability/Player/GameplayAbility_Weapon_Minigun.gd
@@ -1,23 +1,31 @@
 extends "res://src/Ability/Player/GameplayAbility_Weapon_Shotgun.gd"
 
+var CurrentBulletCount = 0
+export var BulletClip = 10
+export var ReloadCooldown = 3.0
 
-#func GetSpawnRotation() -> Vector2:
-#	var vec : Vector2
-#
-#	if is_instance_valid(TargetActor):
-#		vec = (TargetActor.GetTargetingPosition() - GetSpawnPosition()).normalized()
-#	elif SpawnRotation != Vector2(0,0) :
-#		vec = SpawnRotation 
-#	else :
-#		vec = Vector2(AbilityOwner.FacingDirection, 0)
-#
-#		if AbilityOwner is Player : 
-#			if Input.is_action_pressed("shoot") :
-#				if Input.is_action_pressed("move_up") :
-#					vec.x = 0.0
-#					vec.y -= 1.0
-#				if Input.is_action_pressed("move_down") :
-#					vec.x = 0.0
-#					vec.y += 1.0
-#
-#	return vec
+var ReloadTimer: Timer
+var is_reloading = false
+
+func _ready():
+	ReloadTimer = GlobalFunctions.CreateTimerAndBind(self, self, "OnReloadTimerEnd")
+	
+func OnReloadTimerEnd():
+	is_reloading = false
+	CurrentBulletCount = 0
+	
+func IsAbilityOnCooldown() -> bool:
+	return !ReloadTimer.is_stopped() or .IsAbilityOnCooldown()
+	
+func GetAbilityRemainingCooldownSeconds() -> float:
+	if ReloadTimer.is_stopped() : 
+		return .GetAbilityRemainingCooldownSeconds()
+	else : 
+		return ReloadTimer.get_time_left()
+
+func OnSpawnBullet(bullet):
+	CurrentBulletCount += 1
+	if CurrentBulletCount >= BulletClip : 
+		is_reloading = true
+		ReloadTimer.start(ReloadCooldown)
+	pass
