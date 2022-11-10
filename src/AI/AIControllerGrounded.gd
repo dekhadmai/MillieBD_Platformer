ff0a45d6c6d6edc2202a45903500320c476bd245
dekhadmai <bank_kuna@hotmail.com>
2022-11-09 19:55:43 -0500

    Add patrol_offset_x to AI Grounded enemy

--- src/AI/AIControllerGrounded.gd
+++ src/AI/AIControllerGrounded.gd
@@ -5,10 +5,14 @@ extends AIController
 onready var platform_detector = $PlatformDetector
 onready var floor_detector_left = $FloorDetectorLeft
 onready var floor_detector_right = $FloorDetectorRight
+export var patrol_offset_x = 150
+var start_position_x: float
+var bWalkLeft = true
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	start_position_x = get_global_position().x
 	pass # Replace with function body.
 
 
@@ -19,10 +23,19 @@ func _ready():
 func update_physics(delta):
 	if not floor_detector_left.is_colliding():
 		kinematic_body._velocity.x = kinematic_body.speed.x
+		bWalkLeft = false
 	elif not floor_detector_right.is_colliding():
 		kinematic_body._velocity.x = -kinematic_body.speed.x
+		bWalkLeft = true
 
-	if kinematic_body.is_on_wall():
+	if kinematic_body.is_on_wall() :
 		kinematic_body._velocity.x *= -1
+		
+	if get_global_position().x <= start_position_x-patrol_offset_x : 
+		kinematic_body._velocity.x = kinematic_body.speed.x
+		bWalkLeft = false
+	elif get_global_position().x >= start_position_x+patrol_offset_x : 
+		kinematic_body._velocity.x = -kinematic_body.speed.x
+		bWalkLeft = true
 		
 	.update_physics(delta)
