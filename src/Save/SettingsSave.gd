2fb7c6a0683271d7d57bee23f6e1969e6871c6f2
dekhadmai <bank_kuna@hotmail.com>
2022-11-20 03:22:34 -0500

    Fix setting can crash the game

--- src/Save/SettingsSave.gd
+++ src/Save/SettingsSave.gd
@@ -25,6 +25,20 @@ func load_data():
 		save_data()
 	file.open(SAVEFILE, File.READ)
 	game_data = file.get_var()
+	
+	if !game_data.has("vsync_on") :
+		game_data.vysnc_on = false
+	if !game_data.has("fullscreen_on") :
+		game_data.fullscreen_on = false	
+	if !game_data.has("master_vol") :
+		game_data.master_vol = -10
+	if !game_data.has("bgm_vol") :
+		game_data.bgm_vol = -10
+	if !game_data.has("sfx_vol") :
+		game_data.sfx_vol = -10
+	if !game_data.has("hold_to_float") :
+		game_data.hold_to_float = false
+	
 	file.close()
 	
 	
