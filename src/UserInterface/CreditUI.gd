bac587663928b45b2a0a674545c0244c41e214c6
dekhadmai <bank_kuna@hotmail.com>
2022-12-06 05:58:22 -0500

    Add startmenu, Add credit UI

--- src/UserInterface/CreditUI.gd
+++ src/UserInterface/CreditUI.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 const section_time := 2.0
 const line_time := 0.3
-const base_speed := 100
+const base_speed := 50
 const speed_up_multiplier := 10.0
 const title_color := Color.brown
 
@@ -23,45 +23,6 @@ var lines := []
 var bbtext:String = ""
 var current_scroll = 0
 
-var credits = [
-	[
-		"A game by Awesome Game Company"
-	],[
-		"Programming",
-		"Programmer Name",
-		"Programmer Name 2"
-	],[
-		"Art",
-		"Artist Name"
-	],[
-		"Music",
-		"Musician Name"
-	],[
-		"Sound Effects",
-		"SFX Name"
-	],[
-		"Testers",
-		"Name 1",
-		"Name 2",
-		"Name 3"
-	],[
-		"Tools used",
-		"Developed with Godot Engine",
-		"https://godotengine.org/license",
-		"",
-		"Art created with My Favourite Art Program",
-		"https://myfavouriteartprogram.com"
-	],[
-		"Special thanks",
-		"My parents",
-		"My friends",
-		"My pet rabbit"
-	]
-]
-
-
-#func _ready():
-#	$CreditsContainer/RichLine.set_text(CreditText)
 
 func _process(delta):
 		
@@ -82,16 +43,14 @@ func finish():
 	if not finished:
 		finished = true
 		Transition.change_scene("res://src/UserInterface/MainMenu/startmenu.tscn")
-		# NOTE: This is called when the credits finish
-		# - Hook up your code to return to the relevant scene here, eg...
-		#get_tree().change_scene("res://scenes/MainMenu.tscn")
+		
 
 
 func _unhandled_input(event):
 	if event.is_action_pressed("ui_cancel"):
 		finish()
-	if event.is_action_pressed("ui_down") and !event.is_echo():
+	if event.is_action_pressed("ui_accept") and !event.is_echo():
 		speed_up = true
-	if event.is_action_released("ui_down") and !event.is_echo():
+	if event.is_action_released("ui_accept") and !event.is_echo():
 		speed_up = false
 
